f75af857ac4bec7363858109ddc3bfde10be6ac5
zioolek <ziolkowski.tomek@gmail.com>
2017-05-01 20:52:17 +0200

    [ADD] IoTSDK.framework

---
+++ IoTSDK.framework/Headers/IoTSDK.h
@@ -0,0 +1,19 @@
+//
+//  IoTSDK.h
+//  IoTSDK
+//
+//  Created by zioolek on 28/04/2017.
+//  Copyright © 2017 ubudu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IoTSDK.
+FOUNDATION_EXPORT double IoTSDKVersionNumber;
+
+//! Project version string for IoTSDK.
+FOUNDATION_EXPORT const unsigned char IoTSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IoTSDK/PublicHeader.h>
+
+
